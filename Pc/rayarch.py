0987829f4d53eef708d66ff96f69cc833834db15
Adimineman <adiransiska9@gmail.com>
2019-04-15 17:08:00 +0200

    update again again

--- Pc/rayarch.py
+++ Pc/rayarch.py
@@ -3,14 +3,14 @@ import random as rand
 import math as m
 hit = 1e-2
 max_dist = 10
-max_step = 50
+max_step = 100
 
 def toRad(x): return x/360*m.pi*2
 def toDeg(x): return x/(m.pi*2)*360
 def mod(x, y): return (1 if x >= 0 else -1)*(((1 if x >= 0 else -1)*x) % y)
 def map(x, xmin, xmax, ymin, ymax): return (x-xmin)/(xmax-xmin)*(ymax-ymin)+ymin
 
-w = int(720)
+w = int(5000)
 h = int(w/2)
 
 def circle(x, y, z, r): return m.sqrt(x**2+y**2+z**2)-r
@@ -21,54 +21,52 @@ def less(v1, v2): return -(v2+v1)
 def chess(x, y, z, c1, c2): return c1 if (m.floor(x*10)+m.floor(y*10)+m.floor(z*10)) % 2 == 0 else c2
 def mapC(c1, c2, p): return (int(map(p, 0, 1, c1[0], c2[0])), int(map(p, 0, 1, c1[1], c2[1])), int(map(p, 0, 1, c1[2], c2[2])))
 
-camera = {"dir": [toRad(0), toRad(90)], "poz": [0, 0, 0], "fovX": toRad(360), "fovY": toRad(180), "back": lambda x, y, z: (int(rand.random())*255,)*3}
+camera = {"dir": [toRad(0), toRad(90)], "poz": [0, 0, 0], "fovX": toRad(360), "fovY": toRad(180), "back": lambda ray:mapC((255,255,255),(100,50,150),ray.dist/max_dist)}
 objects = [
-    [lambda x, y, z:circle(x-2, ((y+1)%2)-1, z, 1),
-     lambda x, y, z:mapC((225, 125, 200), (125, 240, 225), (y+1) % 2)
+    [lambda ray:circle(ray.poz[0]-2, ((ray.poz[1]+1)%2)-1, ((ray.poz[2]+2)%4)-2, 1),
+     lambda ray:mapC((225, 125, 200), (125, 240, 225), (ray.poz[1]+1) % 4)
      ],
 
-    [lambda x, y, z:more(z, 5),
-     lambda x, y, z:(int(x*x+y*y)*10,)*3
-     # chess(x,y,z,(162,240,235),(94,144,78))
-     ],
-    [lambda x, y, z:less(z, -5),
-     lambda x, y, z:chess(x, y, z, (50, 50, 255), (120, 120, 255))],
+    #[lambda ray:more(ray.poz[2], 5),
+     #lambda ray:(int(ray.poz[0]*ray.poz[0]+ray.poz[1]*ray.poz[1])*10,)*3
+     # chess(ray.poz[0],ray.poz[1],ray.poz[2],(162,240,235),(94,144,78))
+     #],
+    #[lambda ray:less(ray.poz[2], -5),
+     #lambda ray:chess(ray.poz[0], ray.poz[1], ray.poz[2], (50, 50, 255), (120, 120, 255))],
 
-    [lambda x, y, z:more(y, 5),
-     lambda x, y, z:chess(x, y, z, (25, 167, 31), (141, 235, 113))],
-    [lambda x, y, z:less(y, -5),
-     lambda x, y, z:chess(x, y, z, (226, 66, 168), (90, 121, 161))],
+    #[lambda ray:more(ray.poz[1], 5),
+     #lambda ray:chess(ray.poz[0], ray.poz[1], ray.poz[2], (25, 167, 31), (141, 235, 113))],
+    #[lambda ray:less(ray.poz[1], -5),
+     #lambda ray:chess(ray.poz[0], ray.poz[1], ray.poz[2], (226, 66, 168), (90, 121, 161))],
 
-    [lambda x, y, z:more(x, 5),
-     lambda x, y, z:chess(x, y, z, (255, 100, 100), (255, 150, 150))],
-    [lambda x, y, z:less(x, -5),
-     lambda x, y, z:chess(x, y, z, (100, 255, 100), (150, 255, 150))]
+    #[lambda ray:more(ray.poz[0], 5),
+     #lambda ray:chess(ray.poz[0], ray.poz[1], ray.poz[2], (255, 100, 100), (255, 150, 150))],
+    [lambda ray:less(ray.poz[0], -5),
+     lambda ray:chess(ray.poz[0], ray.poz[1], ray.poz[2], (100, 255, 100), (150, 255, 150))]
 ]
 
 class ray:
     color = None
     isHit = 0
-
+    dist = 0
     def __init__(self, poz, direc, back):
         self.poz = poz
         self.Vx = m.sin(direc[1])*m.cos(direc[0])
         self.Vy = m.sin(direc[1])*m.sin(direc[0])
         self.Vz = m.cos(direc[1])
         self.back = back
-        self.dist = 0
 
     def getDist(self, objects):
         minL = 100
         for ob in objects:
-            a = ob[0](self.poz[0], self.poz[1], self.poz[2])
+            a = ob[0](self)
             if a < minL:
                 minL = a
                 if a < hit:
                     self.isHit = 1
-                    self.color = ob[1](self.poz[0], self.poz[1], self.poz[2])
+                    self.color = ob[1](self)
                     break
         self.spd = minL
-        self.dist += minL
 
     def move(self, objects):
         for a in range(max_step):
@@ -81,8 +79,9 @@ class ray:
             self.poz[0] += self.Vx*r
             self.poz[1] += self.Vy*r
             self.poz[2] += self.Vz*r
+            self.dist += self.spd
         if not self.isHit:
-            self.color = self.back(self.poz[0], self.poz[1], self.poz[2])
+            self.color = self.back(self)
 
 def __main__():
     g = Image.new("RGB", (w, h))
@@ -97,6 +96,7 @@ def __main__():
             r.move(objects)
             gc.point((x, y), fill=r.color)
         print(x)
+        if x%500==0:g.save("render.png", "PNG")
     g.save("render.png", "PNG")
     g.save("render.jpg", "JPEG")
 
